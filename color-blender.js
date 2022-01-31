4a645928cd4df535f211ffee7ae3018659ee6edf
Terje Urnes <terje@urnes.net>
2022-01-31 11:31:31 +0100

    Connecting with the input controllers

--- color-blender.js
+++ color-blender.js
@@ -13,6 +13,42 @@ var t79CB = {
 
 function initColorBlender() {
     getElements();
+    setEventListener();
+}
+
+function setColor(e) {
+    if (e.target.id == 'color-picker-1') {
+        t79CB.inputColorText1.value = e.target.value;
+    } else if (e.target.id == 'color-picker-2') {
+        t79CB.inputColorText2.value = e.target.value;
+    } else if (e.target.id == 'color-text-1') {
+        const color = checkThatValueIsAColor(e.target.value);
+        if (color) {
+            t79CB.inputColorPicker1.value = color;
+        } else { return; }
+    } else if (e.target.id == 'color-text-2') {
+        const color = checkThatValueIsAColor(e.target.value);
+        if (color) {
+            t79CB.inputColorPicker2.value = color;
+        } else { return; }
+    }
+}
+
+function checkThatValueIsAColor(value) {
+    if (/^(#{1})([0-9A-F]{6})$/i.test(value)) {
+        return value;
+    }
+    if (/^(#{1})([0-9A-F]{3})$/i.test(value)) {
+        v = value.split('');
+        return '' + v[0] + v[1] + v[1] + v[2] + v[2] + v[3] + v[3];
+    }
+}
+
+function setEventListener() {
+    t79CB.inputColorText1.addEventListener('input', setColor);
+    t79CB.inputColorPicker1.addEventListener('input', setColor);
+    t79CB.inputColorText2.addEventListener('input', setColor);
+    t79CB.inputColorPicker2.addEventListener('input', setColor);
 }
 
 function getElements() {
