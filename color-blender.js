9987f4d4d369fce4ac251a7fab66b28981c3c1d1
Terje Urnes <terje@urnes.net>
2022-01-31 14:02:24 +0100

    merging in color blender v.0.1
    commit 940f69294acba01c0ccb284443704703eb99e3f7
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 13:51:08 2022 +0100
    
        Get the colorname properly placed
    
    commit 797fc37ea6eb2afc53f9b6a91d6248137f15c4e8
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 13:36:23 2022 +0100
    
        version 0.1
    
    commit 1ebdadd001f3f991b6ca7109f8e53ac31d51339a
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 13:33:56 2022 +0100
    
        fixing syntax
    
    commit 800ec37eabf1a17d89ba3eb1f9c350b85bc7c8aa
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 13:31:54 2022 +0100
    
        Setting color values at load
    
    commit ad99e3e1130a4edb9e14f301fc296d6ff692d3b4
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 13:22:13 2022 +0100
    
        Filling the fields with color and adding color value
    
    commit 76e2f4475feaf149e16e9c8aeba8790a2754cf4e
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 12:54:58 2022 +0100
    
        Redraw color fields on resize
    
    commit 98075cd2d3f96d1a1475610c08b2f7f7ced900bf
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 12:52:41 2022 +0100
    
        Draw up color fields on load
    
    commit 8bc8c303058cf4995801d911ebecc169c1e88005
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 12:46:14 2022 +0100
    
        Adding current font size
    
    commit 7b758bd27c538f2cd40343b266d32c896ad76084
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 12:42:18 2022 +0100
    
        Construct the number of fields / color shades
    
    commit 63fb40468aa0fc5882379e14d37ec9463f1615d8
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 12:40:27 2022 +0100
    
        Small adjustments
    
    commit a68df00b4980c3219767b01ceeb1ce362d82932a
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 11:41:19 2022 +0100
    
        Connecting the input slider
    
    commit 4a645928cd4df535f211ffee7ae3018659ee6edf
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 11:31:31 2022 +0100
    
        Connecting with the input controllers
    
    commit 54d18e8ea28d6850aad1c317a7afd53de2334cb9
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 10:57:32 2022 +0100
    
        Get html elements at load
    
    commit a6b30e241f82cc1bc65beb6cac53b3d9cfec56f6
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 10:51:00 2022 +0100
    
        Adding input controller
    
    commit ebb1848e9508480f7e849d2540f561aa929bd618
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 10:41:37 2022 +0100
    
        Basic css formating
    
    commit 08e82cb0d2d9209daa49ebedb1d7af7fb6515984
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 10:22:20 2022 +0100
    
        Adding color library for color conversion
    
    commit 6f759f56d682c5ee18b2d39171478898e5a74948
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 10:17:28 2022 +0100
    
        Adding credits and license
    
    commit 83e4fa8c6bcc6f871b986ba433d93b38a6ea223c
    Author: Terje Urnes <terje@urnes.net>
    Date:   Mon Jan 31 10:15:19 2022 +0100
    
        Seeting up the main webpage structure
    
    commit 4aadce92ddc41abc0d774fcb5294ea0ec6d08021
    Author: Terje Urnes <terje@urnes.net>
    Date:   Sat Jan 29 15:19:59 2022 +0100
    
        Adding the javascript file

---
+++ color-blender.js
@@ -0,0 +1,195 @@
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === 'interactive') {
+        initColorBlender();
+    }
+});
+
+window.addEventListener('resize', function () {
+    getCurrentFontSize();
+    constructOutputFields();
+});
+
+var t79CB = {
+ 
+}
+
+function initColorBlender() {
+    getElements();
+    getCurrentFontSize();
+    setEventListener();
+    setNumberOfShades(null);
+    initColorInputTextfield();
+}
+
+function setColor(e) {
+    if (e.target.id == 'color-picker-1') {
+        t79CB.inputColorText1.value = e.target.value;
+    } else if (e.target.id == 'color-picker-2') {
+        t79CB.inputColorText2.value = e.target.value;
+    } else if (e.target.id == 'color-text-1') {
+        const color = checkThatValueIsAColor(e.target.value);
+        if (color) {
+            t79CB.inputColorPicker1.value = color;
+        } else { return; }
+    } else if (e.target.id == 'color-text-2') {
+        const color = checkThatValueIsAColor(e.target.value);
+        if (color) {
+            t79CB.inputColorPicker2.value = color;
+        } else { return; }
+    }
+
+    setColorShades();
+}
+
+function checkThatValueIsAColor(value) {
+    if (/^(#{1})([0-9A-F]{6})$/i.test(value)) { // #081d58
+        return value;
+    }
+    if (/^(#{1})([0-9A-F]{3})$/i.test(value)) { // #126
+        v = value.split('');
+        return '' + v[0] + v[1] + v[1] + v[2] + v[2] + v[3] + v[3];
+    }
+}
+
+function setNumberOfShades(e) {
+    if (e == null) {
+        setupOutputFieldsTable(5);
+    } else {
+        setupOutputFieldsTable(parseInt(e.target.value));
+    }
+}
+
+function setupOutputFieldsTable(numberOfShades) {
+    t79CB.outputColorField.innerHTML = '';
+    t79CB.outputFields = Array(numberOfShades).fill();
+    for (fieldIndex in t79CB.outputFields) {
+        let field = {};
+        
+        const svgContainer = document.createElement('div');
+        svgContainer.classList.add('svg-container-main-view');
+        field['svgContainerMainView'] = svgContainer;
+
+        const textPartContainer = document.createElement('div');
+        textPartContainer.classList.add('text-part-container');
+        const textField = document.createElement('span');
+        textField.classList.add('color-text-field');
+        textPartContainer.appendChild(textField);
+        field['textField'] = textField;
+
+        const fieldContainer = document.createElement('div');
+        fieldContainer.classList.add('color-container');
+        fieldContainer.appendChild(svgContainer);
+        fieldContainer.appendChild(textPartContainer);
+
+        t79CB.outputColorField.appendChild(fieldContainer);
+        t79CB.outputFields[fieldIndex] = field;
+    }
+    constructOutputFields()
+} 
+
+function constructOutputFields() {
+
+    const outputDivWidth = t79CB.outputColorField.clientWidth;
+
+    const standarFieldHeight = t79CB.curentFontSize * 4;
+    const minStandardFieldHeight = t79CB.curentFontSize * 1.8;
+
+    const fieldHeight = interpolation(t79CB.outputFields.length, 3, standarFieldHeight, 20, minStandardFieldHeight);
+    const fieldWidth = outputDivWidth - 100;
+
+    for (fieldIndex in t79CB.outputFields) {
+        const colorField = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+        colorField.setAttribute('width', String(fieldWidth));
+        colorField.setAttribute('height', String(fieldHeight));
+
+        const colorRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        colorRect.classList.add('color-rect');
+        colorRect.setAttributeNS(null, 'width', String(fieldWidth));
+        colorRect.setAttributeNS(null, 'height', String(fieldHeight));
+        colorRect.style.fill = '#ffffff';
+        colorField.appendChild(colorRect);
+
+        t79CB.outputFields[fieldIndex]['svgContainerMainView'].innerHTML = '';
+        t79CB.outputFields[fieldIndex]['svgContainerMainView'].appendChild(colorField);
+        t79CB.outputFields[fieldIndex]['svgContainerMainView'].setAttribute('width', String(fieldWidth) + 'px');
+        t79CB.outputFields[fieldIndex]['svgContainerMainView'].style.height = String(fieldHeight) + 'px';
+    }
+
+    setColorShades();
+}
+
+function setColorShades() {
+
+    const value1 = t79CB.inputColorPicker1.value;
+    const rect1 = t79CB.outputFields[0]['svgContainerMainView'].querySelector('.color-rect');
+    const color1 = w3color(value1);
+    const rgbColor1Str = color1.toRgbString();
+    rect1.style.fill = rgbColor1Str;
+
+    const value2 = t79CB.inputColorPicker2.value;
+    const rect2 = t79CB.outputFields[t79CB.outputFields.length - 1]['svgContainerMainView'].querySelector('.color-rect');
+    const color2 = w3color(value2);
+    const rgbColor2Str = color2.toRgbString();
+    rect2.style.fill = rgbColor2Str;
+
+    const rgbColor1 = color1.toRgb();
+    const rgbColor2 = color2.toRgb();
+
+    const stepR = (rgbColor2.r - rgbColor1.r) / (t79CB.outputFields.length - 1);
+    const stepG = (rgbColor2.g - rgbColor1.g) / (t79CB.outputFields.length - 1);
+    const stepB = (rgbColor2.b - rgbColor1.b) / (t79CB.outputFields.length - 1);
+
+    for (colorIndex in t79CB.outputFields) {
+        const rectMain = t79CB.outputFields[colorIndex]['svgContainerMainView'].querySelector('.color-rect');
+        color = {};
+
+        color['r'] = rgbColor1.r + stepR * colorIndex;
+        color['g'] = rgbColor1.g + stepG * colorIndex;
+        color['b'] = rgbColor1.b + stepB * colorIndex;
+
+        const rgbColor = 'rgb(' + color['r'] + ',' + color['g'] + ',' + color['b'] + ')';
+
+        const outputColor = w3color(rgbColor);
+
+        rectMain.style.fill = rgbColor;
+
+        t79CB.outputFields[colorIndex]['textField'].innerHTML = '<span>' + outputColor.toHexString() + '</span>';
+        
+        console.log(t79CB.outputFields[colorIndex]['textField'].clientWidth);
+        
+    }
+
+}
+
+function initColorInputTextfield() {
+    t79CB.inputColorText1.value = t79CB.inputColorPicker1.value;
+    t79CB.inputColorText2.value = t79CB.inputColorPicker2.value;
+}
+
+function setEventListener() {
+    t79CB.inputNumberOfShadesSlider.addEventListener('input', setNumberOfShades);
+    t79CB.inputColorText1.addEventListener('input', setColor);
+    t79CB.inputColorPicker1.addEventListener('input', setColor);
+    t79CB.inputColorText2.addEventListener('input', setColor);
+    t79CB.inputColorPicker2.addEventListener('input', setColor);
+}
+
+function getElements() {
+    t79CB.outputColorField = document.getElementById('output-colors');
+    t79CB.inputNumberOfShadesSlider = document.getElementById('number-of-shades-slider');
+    t79CB.inputColorText1 = document.getElementById('color-text-1');
+    t79CB.inputColorPicker1 = document.getElementById('color-picker-1');
+    t79CB.inputColorText2 = document.getElementById('color-text-2');
+    t79CB.inputColorPicker2 = document.getElementById('color-picker-2');
+    t79CB.htmlElement = document.querySelector('html');
+}
+
+function interpolation(x, x1, y1, x2, y2) {
+    return y1 + ((x - x1)*(y2-y1))/(x2-x1);
+}
+
+function getCurrentFontSize() {
+    var fontSizeString = window.getComputedStyle(t79CB.htmlElement, null).getPropertyValue('font-size');
+    t79CB.curentFontSize = parseFloat(fontSizeString);
+}
+
